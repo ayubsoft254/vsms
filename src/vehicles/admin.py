295adc86b945f5a6323cb18f18c1a13ee372aa7e
ayubsoft254 <henryoayub15@gmail.com>
2025-08-05 23:30:22 +0300

    Register Vehicle model in admin with custom admin interface

--- src/vehicles/admin.py
+++ src/vehicles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vehicle
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'year', 'price', 'status')
+    search_fields = ('make', 'model', 'VIN')
+    list_filter = ('status', 'year', 'make')
+    ordering = ('-year', 'make', 'model')
+    fieldsets = (
+        (None, {
+            'fields': ('make', 'model', 'VIN', 'year', 'specs', 'price', 'mileage', 'color', 'photos', 'status')
+        }),
+    )
